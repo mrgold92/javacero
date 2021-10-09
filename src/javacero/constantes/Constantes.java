c3218e1315daa91afdf23fbabdef4b273eeb852a
David Salazar <davids1_4@hotmail.com>
2021-10-09 15:09:43 +0200

    Modificaciones en Constantes

--- src/javacero/constantes/Constantes.java
+++ src/javacero/constantes/Constantes.java
@@ -17,8 +17,8 @@ public class Constantes {
         final int IVA = 21;
         System.out.println(IVA);
 
-        IVA = 30;  // Error: cannot asign a value to final variable IVA
-        System.out.println(IVA);
+        // IVA = 30; Error: cannot asign a value to final variable IVA
+        // System.out.println(IVA);
     }
 
 }
