7cd880bf901434c404d6f8d52bc891a8d6a9b6c6
David Salazar <davids1_4@hotmail.com>
2021-10-09 14:24:40 +0200

    First Commit

---
+++ src/javacero/estructura/Principal.java
@@ -0,0 +1,40 @@
+package javacero.estructura;
+
+/**
+ *
+ * Las estructura básica de un archivo .java es la siguiente: Java se estructura
+ * mediante la programación orientada a objetos, eso quiere decir que todo está
+ * compuesto o conformado en clases.
+ *
+ * Las clases en java se declaran poniendo: 
+ * public class NombreClase {
+ *      Código...
+ * }
+ *
+ * Todo lo que desarrollemos irá dentro de las llaves de apertura y cierre de la
+ * clase.
+ * 
+ * Los comentarios, que son porciones de texto que nunca se ejecutan, simplemente
+ * se usan para dar información o explicar algo se describen así:
+ * 
+ * - Comentarios de una linea: // El comentario
+ * - Comentarios de varias líneas: / * 
+                                     * El comentario 
+                                     * /
+ * (Sin espacios)
+ *
+ * @author David Salazar
+ */
+public class Principal {
+
+    // Este es el método principal, eso quiere decir, que todo el proyecto
+    // iniciará aquí. La máquina virtual de java buscará siempre este método
+    // para ejecutarlo en primer lugar, y a partir de aquí, ya se hace lo demás
+    // que hayamos programado. Es obligatorio escribirlo tal cual.
+    
+    
+    public static void main(String[] args) {
+        // Desde aquí ya podemos programar
+    }
+
+}
