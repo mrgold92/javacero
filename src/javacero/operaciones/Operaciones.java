7e9cf4807b07161d398a86b6f249119bac281406
David Salazar <davids1_4@hotmail.com>
2021-10-09 15:14:54 +0200

    Añadido Operaciones

---
+++ src/javacero/operaciones/Operaciones.java
@@ -0,0 +1,23 @@
+package javacero.operaciones;
+
+/**
+ *
+ * @author David
+ */
+public class Operaciones {
+
+    
+    public static void main(String[] args) {
+        //Con valores numéricos podemos hacer operaciones:
+        
+        System.out.println(2+2);    //4
+        System.out.println(5-1);    //4
+        System.out.println(7/2);    //3
+        
+        int numero1 = 40;
+        int numero2 = 8;
+        
+        System.out.println(numero1 + numero2); //48
+    }
+
+}
