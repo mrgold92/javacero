2559aee2ed6565ecce26ca5f9b73997ebe63844f
David Salazar <davids1_4@hotmail.com>
2021-10-09 15:04:52 +0200

    Añadido Constantes

---
+++ src/javacero/constantes/Constantes.java
@@ -0,0 +1,24 @@
+package javacero.constantes;
+
+/**
+ *
+ * A diferencia de las variables, las constantes no pueden cambiar su valor,
+ * esto se hace para proteger algunas variables para que a lo largo de la
+ * ejecución del programa, su valor no pueda cambiar.
+ *
+ * Por ejemplo si guardamos el IVA. Pòr convención el nombre de la constante se
+ * escribe en MAYÚSCULA.
+ *
+ * @author David Salazar
+ */
+public class Constantes {
+
+    public static void main(String[] args) {
+        final int IVA = 21;
+        System.out.println(IVA);
+
+        IVA = 30;  // Error: cannot asign a value to final variable IVA
+        System.out.println(IVA);
+    }
+
+}
